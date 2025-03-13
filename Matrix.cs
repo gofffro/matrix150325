63db091cf97f434b196ad91bb3035dfd5f76bef9
Stepan Vazhenin <vazheninstepan@gmail.com>
2025-03-13 20:50:49 +0700

    создал конструктор, который задаёт размеры матрицы и инициализирует массив

---
+++ Matrix.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatrixApp
+{
+  public class Matrix
+  {
+    int matrixRow = 3;
+    int matrixColumn = 3;
+    int matrixSize = 9;
+    int[,] matrix;
+    public Matrix(int matrixRow, int matrixColumn) 
+    {
+      this.matrixRow = matrixRow;
+      this.matrixColumn = matrixColumn;
+      matrix = new int[matrixRow, matrixColumn];
+    }
+  }
+}
