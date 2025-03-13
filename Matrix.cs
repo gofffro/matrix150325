1d00cf446ebaf371eb2e593a26e5256fa3664287
Stepan Vazhenin <vazheninstepan@gmail.com>
2025-03-13 22:11:59 +0700

    Перегружены операторы + и * для матриц

--- Matrix.cs
+++ Matrix.cs
@@ -52,5 +52,54 @@ namespace MatrixApp
         Console.WriteLine();
       }
     }
+
+    public static Matrix operator +(Matrix matrixA,  Matrix matrixB)
+    {
+      if (matrixA.matrixRow != matrixB.matrixRow || matrixA.matrixColumn != matrixB.matrixColumn)
+      {
+        throw new ArgumentException("Только матрицы одного размера, можно складывать");
+      }
+
+      Matrix matrixResult = new Matrix(matrixA.matrixRow, matrixB.matrixRow);
+
+      int matrixRow = matrixA.matrixRow;
+      int matrixColumn = matrixB.matrixColumn;
+
+      for (int row = 0; row < matrixRow; ++row)
+      {
+        for (int column = 0; column < matrixColumn; ++column)
+        {
+          matrixResult.matrix[row, column] = matrixA.matrix[row, column] + matrixB.matrix[row, column];
+        }
+      }
+
+      return matrixResult;
+    }
+
+    public static Matrix operator *(Matrix matrixA, Matrix matrixB)
+    {
+      if (matrixA.matrixColumn != matrixB.matrixRow)
+      {
+        throw new ArgumentException("Количество столбцов в первой матрице должно равняться количеству строк во втором");
+      }
+
+      Matrix matrixResult = new Matrix(matrixA.matrixRow, matrixB.matrixRow);
+
+      int matrixRow = matrixA.matrixRow;
+      int matrixColumn = matrixB.matrixColumn;
+
+      for (int row = 0; row < matrixRow; ++row)
+      {
+        for (int column = 0; column < matrixColumn; ++column)
+        {
+          for (int sumIndex = 0; sumIndex < matrixA.matrixColumn; ++sumIndex)
+          {
+            matrixResult.matrix[row, column] += matrixA.matrix[row, sumIndex] * matrixB.matrix[sumIndex, column];
+          }
+        }
+      }
+
+      return matrixResult;
+    }
   }
 }
